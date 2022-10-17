076b72c7f1f6a4a6d86685b699c49ea5ed9c53c1
netrahindocha <netrahindocha1@gmail.com>
2022-10-17 18:12:11 +0530

    Creating triangular borders in homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,11 +8,12 @@ function Home() {
         <div className="triangles flexRow justifyCenter alignCenter">
             <div className="one flexCol">
                 {/* <img src="https://images.unsplash.com/photo-1665925246453-e7aeb96b3744?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" /> */}
-                <img src="https://images.unsplash.com/photo-1560363199-a1264d4ea5fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="" />
-                <span className="borderOne"></span>
+                {/* <img src="https://images.unsplash.com/photo-1579187707643-35646d22b596?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80" alt="" /> */}
+                <img src="https://images.unsplash.com/photo-1557576146-047908becbb4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=436&q=80" alt="" />
             </div>
-            <div className="two">
+            <div className="two flexCol">
                 {/* <img src="https://images.unsplash.com/photo-1665922075713-a20594664b4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80" alt="" /> */}
+                {/* <img src="https://images.unsplash.com/photo-1515266591878-f93e32bc5937?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="" /> */}
                 <img src="https://images.unsplash.com/photo-1567254790685-6b6d6abe4689?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" alt="" />
             </div>
             <div className="flexCol">
@@ -32,11 +33,20 @@ function Home() {
             <div className="six">
                 {/* <img src="https://images.unsplash.com/photo-1665979154453-ceb2a4934028?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="" /> */}
                 <img src="https://images.unsplash.com/photo-1484335629320-0e089b87a106?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=763&q=80" alt="" />
+
             </div>
             <div className="seven">
                 {/* <img src="https://images.unsplash.com/photo-1665996977813-ee520a6608ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1964&q=80" alt="" /> */}
                 <img src="https://images.unsplash.com/photo-1543893490-4deb3431e479?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80" alt="" />
             </div>
+
+            <div className="borderOne"></div>
+            <div className="borderTwo"></div>
+            <div className="borderThree"></div>
+            <div className="borderFour"></div>
+            <div className="borderSix"></div>
+            <div className="borderFive"></div>
+            <div className="borderSeven"></div>
         </div>
     </div>
     </>
