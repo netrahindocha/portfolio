2ec340b741907b7574d259bca038ad95432dadc5
netrahindocha <netrahindocha1@gmail.com>
2022-10-17 16:15:56 +0530

    Creating triangles design in HomePage

---
+++ src/components/Home.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import '../styling/Home.css'
+
+function Home() {
+  return (
+    <>
+    <div className="container justifyCenter alignCenter">
+        <div className="triangles flexRow justifyCenter alignCenter">
+            <div className="one flexCol">
+                {/* <img src="https://images.unsplash.com/photo-1665925246453-e7aeb96b3744?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" /> */}
+                <img src="https://images.unsplash.com/photo-1560363199-a1264d4ea5fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="" />
+                <span className="borderOne"></span>
+            </div>
+            <div className="two">
+                {/* <img src="https://images.unsplash.com/photo-1665922075713-a20594664b4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80" alt="" /> */}
+                <img src="https://images.unsplash.com/photo-1567254790685-6b6d6abe4689?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" alt="" />
+            </div>
+            <div className="flexCol">
+            <div className="three">
+                {/* <img src="https://images.unsplash.com/photo-1665909235925-4ecc57b25249?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=395&q=80" alt="" /> */}
+                <img src="https://images.unsplash.com/photo-1602427168282-3c023b9d8c6d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="" />
+            </div>
+            <div className="four">
+                {/* <img src="https://images.unsplash.com/photo-1665846589489-5856db428a85?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80" alt="" /> */}
+                <img src="https://images.unsplash.com/photo-1566249827553-6b5276ff157e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" />
+            </div>
+            </div>
+            <div className="five">
+                {/* <img src="https://images.unsplash.com/photo-1665912210382-47b3b8b73466?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="" /> */}
+                <img src="https://images.unsplash.com/photo-1562791241-122a7c501434?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80" alt="" />
+            </div>
+            <div className="six">
+                {/* <img src="https://images.unsplash.com/photo-1665979154453-ceb2a4934028?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="" /> */}
+                <img src="https://images.unsplash.com/photo-1484335629320-0e089b87a106?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=763&q=80" alt="" />
+            </div>
+            <div className="seven">
+                {/* <img src="https://images.unsplash.com/photo-1665996977813-ee520a6608ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1964&q=80" alt="" /> */}
+                <img src="https://images.unsplash.com/photo-1543893490-4deb3431e479?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80" alt="" />
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Home
